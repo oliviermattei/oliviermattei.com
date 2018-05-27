a0b6e16749d563d4112ecb89f33712fca111e863
Olivier Mattei <bonjour@oliviermattei.com>
2018-05-27 22:46:04 +0200

    '.'

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,46 @@
+const path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
 module.exports = {
     entry: [
-        './src/index.js'
+        './src/js/index.js'
     ],
     output: {
-        path: __dirname + '/dist',
-        publicPath: '/',
-        filename: 'bundle.js'
+        path: path.join(__dirname, "dist"),
+        publicPath: '/'
     },
-    devServer: {
-        contentBase: './dist'
-    }
+    watch: true,
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            }
+        ],
+        rules: [{
+            test: /\.scss$/,
+            use: [
+                // fallback to style-loader in development
+                process.env.NODE_ENV !== 'production' ? 'style-loader' : MiniCssExtractPlugin.loader,
+                "css-loader",
+                "sass-loader"
+            ]
+        }]
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: "[name].css",
+            chunkFilename: "[id].css"
+        })
+    ]
+    // devServer: {
+    //     contentBase: path.join(__dirname, "dist"),
+    //     watchContentBase: true,
+    //     open: true,
+    //     hot: true
+    // },
+
 };
